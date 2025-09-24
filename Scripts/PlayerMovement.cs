291b2790361747e9260b11a29b8d63967f762e50
손성현 <thstjdgus0813@gmail.com>
2025-09-24 23:43:23 +0900

    첫커밋

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    [SerializeField] public float speed = 10f;
+    [SerializeField] public float jumpHeight = 3f;
+    [SerializeField] public float dash =  5f;
+    [SerializeField] public float rotSpeed =  3f;
+    private Vector3 dir = Vector3.zero;
+
+    private bool Ground = false;
+    [SerializeField] public LayerMask layer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+        dir.Normalize();
+        
+        checkGround();
+        
+        if (Input.GetButtonDown("Jump") && Ground)
+        {
+            Vector3 jumpPower = Vector3.up * jumpHeight;
+            rb.AddForce(jumpPower,ForceMode.VelocityChange);
+        }
+        if (Input.GetButtonDown("Dash") && Ground)
+        {
+            Vector3 dashPower = this.transform.forward * dash;
+            rb.AddForce(dashPower, ForceMode.VelocityChange);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (dir != Vector3.zero)
+        {
+            if (Mathf.Sign(transform.forward.x) != Mathf.Sign(dir.x) || Mathf.Sign(transform.forward.z) != Mathf.Sign(dir.z))
+            {
+                transform.Rotate(0, 1, 0);
+            }
+            transform.forward = Vector3.Lerp(transform.forward, dir, rotSpeed * Time.deltaTime);
+        }
+        
+        rb.MovePosition(this.gameObject.transform.position + dir * speed * Time.deltaTime);
+    }
+
+    void checkGround()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.2f), Vector3.down, out hit, 0.4f, layer))
+        {
+            Ground = true;
+        }
+        else
+        {
+            Ground = false;
+        }
+        
+    }
+}
