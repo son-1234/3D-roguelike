f22a2197780e58e22a1e2942ffcd576983a4b543
손성현 <thstjdgus0813@gmail.com>
2025-09-26 22:00:15 +0900

    그래플링 훅 제작 재트백 만들어볼 예정

--- Scripts/RopeAction.cs
+++ Scripts/RopeAction.cs
@@ -7,8 +7,10 @@ public class RopeAction : MonoBehaviour
     private RaycastHit hit;
     public LayerMask GrapplingObj;
     private LineRenderer lr;
-
+    private SpringJoint sj;
     private bool OnGrappling = false;
+    public Transform gunTip;
+    private Vector3 spot;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -36,10 +38,23 @@ public class RopeAction : MonoBehaviour
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 100f, GrapplingObj))
         {
             OnGrappling = true;
+
+            spot = hit.point;
             
             lr.positionCount = 2;
             lr.SetPosition(0, transform.position);
             lr.SetPosition(1, hit.point);
+
+            sj = player.gameObject.AddComponent<SpringJoint>();
+            sj.autoConfigureConnectedAnchor = false;// 이게 활성화 되어있으면 자동으로 곗ㄴ해주는데 우리는 직접 계산
+            sj.connectedAnchor = spot;
+
+            float dis = Vector3.Distance(this.transform.position, spot);
+
+            sj.maxDistance = dis;
+            sj.minDistance = dis * 0.5f;
+            sj.spring = 5f;
+            sj.damper = 5f;//여기서 프로퍼티를 값을 바꿔서 화면을 바꿀 수 있음
         }
     }
 
@@ -47,15 +62,15 @@ public class RopeAction : MonoBehaviour
     {
         OnGrappling = false;
         lr.positionCount = 0;
-
-        OnGrappling = false;
+        Destroy(sj);
     }
 
     void DrawRope()
     {
         if (OnGrappling)
         {
-            lr.SetPosition(0, this.transform.position);
+            lr.SetPosition(0, gunTip.position);
+            this.transform.LookAt(spot);
         }
     }
 }
