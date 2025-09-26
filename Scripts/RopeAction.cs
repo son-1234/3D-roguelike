addb32e2f220a8bb2c4da383a0b99e2284340ca9
손성현 <thstjdgus0813@gmail.com>
2025-09-26 19:54:41 +0900

    로프 제작 움직이는거 제작x

--- Scripts/RopeAction.cs
+++ Scripts/RopeAction.cs
@@ -6,10 +6,14 @@ public class RopeAction : MonoBehaviour
     private Camera cam;
     private RaycastHit hit;
     public LayerMask GrapplingObj;
+    private LineRenderer lr;
+
+    private bool OnGrappling = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = Camera.main;
+        lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -19,13 +23,39 @@ public class RopeAction : MonoBehaviour
         {
             RopeShoot();
         }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            EndShoot();
+        }
+        
+        DrawRope();
     }
 
     void RopeShoot()
     {
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 100f, GrapplingObj))
         {
-            print("장애물 걸림");
+            OnGrappling = true;
+            
+            lr.positionCount = 2;
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, hit.point);
+        }
+    }
+
+    void EndShoot()
+    {
+        OnGrappling = false;
+        lr.positionCount = 0;
+
+        OnGrappling = false;
+    }
+
+    void DrawRope()
+    {
+        if (OnGrappling)
+        {
+            lr.SetPosition(0, this.transform.position);
         }
     }
 }
